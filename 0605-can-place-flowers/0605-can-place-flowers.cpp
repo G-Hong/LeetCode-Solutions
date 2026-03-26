02cc23eb843111311396cf5847f29c4fa512de8f
Gina_Hong <128670056+G-Hong@users.noreply.github.com>
2026-03-26 18:40:52 +0900

    Time: 0 ms (100%), Space: 24.9 MB (2.86%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        //attatch 0 infront and back of flowerbed
+        //count consecutive 0 and make as array cnt
+        //-1 to all cnt values and divide with 2
+        //if total num of values in cnt smaller than n, false else True
+        int t = 0, tot = 0;
+        vector<int> cnt;
+        flowerbed.push_back(0);
+        flowerbed.insert(flowerbed.begin(),0);
+        for (int f : flowerbed){
+            if(f == 0){
+                t++;
+            }
+            else{
+                cnt.push_back(t);
+                t=0;
+            }
+        }
+        cnt.push_back(t);
+
+        for (int c : cnt){
+            tot += (c-1)/2;
+        }
+        if (tot<n) return false;
+        else return true;
+
+
+    }
+};
