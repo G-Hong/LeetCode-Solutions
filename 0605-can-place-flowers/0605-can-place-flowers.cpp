833333c08ae23c9c3a1e0238800b776ea2a0e8f2
Gina_Hong <128670056+G-Hong@users.noreply.github.com>
2026-03-26 19:16:49 +0900

    Time: 0 ms (100%), Space: 24.2 MB (42.81%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
@@ -27,5 +28,24 @@ public:
         else return true;
 
 
+    }
+};
+*/
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        //Greedy Algorithm
+
+        for (int i=0; i<flowerbed.size(); i++){
+            if(n==0){
+                return true;
+            }
+            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==flowerbed.size()-1 || flowerbed[i+1]==0)){
+                flowerbed[i] = 1;
+                n -= 1;
+            }
+        }
+        return n<=0;
     }
 };
